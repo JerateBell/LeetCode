6b6cd77de10ab59e5f640d591d080993c372a8fc
ShyYigII <DungDT.B21CN263@stu.ptit.edu.vn>
2025-10-02 09:06:12 +0700

    [01/10/2025] refactor code

--- src/main/java/LeetCode/BinarySearch/GuessNumberHigherOrLower/App.java
+++ src/main/java/LeetCode/BinarySearch/GuessNumberHigherOrLower/App.java
@@ -9,12 +9,11 @@ public class App {
         while (low <= high) {
             mid = low + (high - low) / 2;
 
-            if (guess(mid) == 0)
-                return mid;
-            if (guess(mid) == -1)
-                high = mid - 1;
-            if (guess(mid) == 1)
-                low = mid + 1;
+            if (guess(mid) == 0) return mid;
+
+            if (guess(mid) == -1) high = mid - 1;
+
+            if (guess(mid) == 1) low = mid + 1;
         }
 
         return -1;
