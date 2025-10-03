7e6caba38ffe638f017b1a893fe0269869abc2a2
ShyYigII <DungDT.B21CN263@stu.ptit.edu.vn>
2025-10-03 09:13:04 +0700

    [10/03/2025] Add SameTree code

--- src/main/java/LeetCode/Tree/SameTree/App.java
+++ src/main/java/LeetCode/Tree/SameTree/App.java
@@ -1,4 +1,11 @@
 package LeetCode.Tree.SameTree;
 
 public class App {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+
+        if(p == null || q == null || p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 }
